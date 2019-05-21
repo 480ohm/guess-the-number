d4c876c96af1a0f9c5796682e38f9c4b185f6d73
Richard Wagner <50829371+480ohm@users.noreply.github.com>
2019-05-21 22:03:27 +1000

    Removed printing of number and fixed string conversion error

--- gtn.py
+++ gtn.py
@@ -6,8 +6,6 @@ print("The number will be anywhere from 1 to 100.\n")
 number = randint(1,100)
 num_of_guesses = 0
 
-print(number)
-
 while True:
     num_of_guesses += 1
     guess = input("Guess " + str(num_of_guesses) + ": ")
@@ -17,7 +15,7 @@ while True:
                 print("Congratulations! You guessed the number in 1 try!\n")
             else:
                 print("Congratulations! You guessed the number in " +
-                      num_of_guesses + " tries\n.")
+                      str(num_of_guesses) + " tries\n.")
             break
         elif int(guess) < number:
             print("Your guess is too low. Try again.\n")
